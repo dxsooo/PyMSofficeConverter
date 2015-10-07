3b09ca050fcae17bd83adb58fb91f7e0e5b24afe
hanjiajie01@sina.com <hanjiajie01@sina.com>
2015-10-07 11:49:36 +0800

    ver 0.1

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ Your PC should be WIN7 or above and make sure that you have Microsoft Office(ver
 
 ## Features  
 support Microsoft word document convert between **.doc** and **.docx**
+support Microsoft powerpoint presentation convert between **.ppt** and **.pptx**
 
 ## Usage
 copy the necessary code in the files
 
 ## Documentation
-See code detail in file 'word.py'
+See code detail in the files
 
 ### Updates
 ver 0.1 on 2015/10/7
---
+++ powerpoint.py
@@ -0,0 +1,20 @@
+from win32com import client as wc
+import os
+powerpoint = wc.Dispatch('PowerPoint.Application')
+# format code https://msdn.microsoft.com/ZH-CN/library/office/ff746500.aspx
+format_dict={'ppt':1,'pptx':11}
+def convert(fromdir,todir):
+    ppt = powerpoint.Presentations.Open(fromdir)
+    file_type=''
+    if todir.rfind('.')==-1:
+        file_type='pptx'
+        todir=todir+'.pptx'
+    else:
+        file_type=todir[todir.rfind('.')+1:]
+    if format_dict.has_key(file_type):
+        ppt.SaveAs(todir,format_dict[file_type])
+    ppt.Close()
+
+homedir = os.getcwd()+'\\'
+convert(homedir+'test2.pptx',homedir+'tst.ppt')
+powerpoint.Quit()
